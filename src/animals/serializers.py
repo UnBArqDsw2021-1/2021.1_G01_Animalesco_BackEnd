7585c5eca633e61984346910820c4a505638eac1
Durval Carvalho <durvalcsouza@outlook.com>
2021-09-08 23:11:38 -0300

    Add animal serializers
    Co-authored-by: Lorrany Souza <lolo.souza80@yahoo.com>

--- src/animals/serializers.py
+++ src/animals/serializers.py
@@ -1,8 +1,46 @@
+
 from rest_framework import serializers
-from animals.models import Specie
+
+from animals.models import Pet, Breed, Specie
 
 
 class SpecieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Specie
-        fields = ['id', 'name',]
+        fields = [ 'id', 'name', ]
+
+
+class BreedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Breed
+        fields = [ 'id', 'name', ]
+
+
+class PetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pet
+        fields = [
+            'id',
+            'name',
+            'sex',
+            'breed',
+            'birth_date',
+            'is_neutered',
+            'color',
+        ]
+
+        # extra_kwargs é um atributo das ModelSerializers para sobrescrever as mensagens
+        # padrões do django rest framework.
+        extra_kwargs = {
+            'sex': {
+                'error_messages': {
+                    # 'invalid_choice': "Please provide a valid sex:  ('M', 'F')",
+                    'invalid_choice': f"Please provide a valid sex: {Pet.get_valid_sex_options()}",
+                }
+            },
+            'breed': {
+                'error_messages': {
+                    'null': "Please provide a valid breed ID",
+                }
+            },
+        }
