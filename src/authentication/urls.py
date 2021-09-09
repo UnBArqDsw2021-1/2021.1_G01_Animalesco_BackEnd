a01fb878e71dce44c4d3d9637e2b85b0895ce20e
Durval Carvalho <durvalcsouza@outlook.com>
2021-09-08 23:18:21 -0300

    Add Token auth

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # https://djoser.readthedocs.io/en/latest/getting_started.html
 
     re_path(r'', include('djoser.urls')),
+    re_path(r'', include('djoser.urls.authtoken')),
 
     # jwt/create/     # jwt/refresh/     # jwt/verify/
     path(r'', include('djoser.urls.jwt')),
