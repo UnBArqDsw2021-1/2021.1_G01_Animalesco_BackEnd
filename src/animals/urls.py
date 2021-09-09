756dc3b6697fb1bb55d7ff15fc75f35aa67070c0
Durval Carvalho <durvalcsouza@outlook.com>
2021-09-08 23:12:08 -0300

    Add animals routers
    Co-authored-by: Lorrany Souza <lolo.souza80@yahoo.com>

--- src/animals/urls.py
+++ src/animals/urls.py
@@ -1,12 +1,39 @@
 from django.urls import include, path
 
-from rest_framework import routers
-from animals.views import SpecieViewSet
+from rest_framework_nested import routers
+
+
+from .views import (
+    PetViewSet,
+    SpecieViewSet,
+    BreedViewSet,
+    get_all_specie_breeds,
+)
+
+app_name = 'pets'
 
 router = routers.DefaultRouter()
-router.register('species', SpecieViewSet)
 
+router.register(r'pets', PetViewSet)
+router.register(r'species', SpecieViewSet)
+
+# Aqui é onde criamos rotas nested. Na linha abaixo estamos criando um roteador dentro
+# do contexto de species
+specie_router = routers.NestedDefaultRouter(
+    router,
+    r"species",
+    lookup="specie",
+)
+
+# Aqui estamos colocando as rotas de breeds dentro do contexto de species
+specie_router.register(
+    r"breeds",
+    BreedViewSet,
+    basename='breeds'
+)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("species-breeds/", get_all_specie_breeds),
+    path("", include(router.urls)),
+    path("", include(specie_router.urls)),
 ]
