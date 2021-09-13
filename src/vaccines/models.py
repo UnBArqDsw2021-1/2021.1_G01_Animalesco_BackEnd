7d336770741787e6735e7f8bdfffe3ec0e39502b
Durval Carvalho <durvalcsouza@outlook.com>
2021-09-13 12:21:37 -0300

    #124 improve models module readability

--- src/vaccines/models.py
+++ src/vaccines/models.py
@@ -1,11 +1,32 @@
 from django.db import models
 from animals.models import Pet
 
+
 class Vaccine(models.Model):
-    name = models.CharField(verbose_name="Vaccine name", max_length=100, null=False, blank=False)
-    application_date = models.DateField(verbose_name="Dose application date", null=False, blank=False)
-    next_application_date = models.DateField(verbose_name="Next dose application date", null=True, blank=True)
-    pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name="vaccines")
+    name = models.CharField(
+        verbose_name="Vaccine name",
+        max_length=100,
+        null=False,
+        blank=False,
+    )
+
+    application_date = models.DateField(
+        verbose_name="Dose application date",
+        null=False,
+        blank=False,
+    )
+
+    next_application_date = models.DateField(
+        verbose_name="Next dose application date",
+        null=True,
+        blank=True,
+    )
+
+    pet = models.ForeignKey(
+        Pet,
+        on_delete=models.CASCADE,
+        related_name="vaccines",
+    )
 
     # Para que seja possível realizar um melhor controle gráfico das vacinas, este campo booleano informa se a vacina terá ou não
     # próxima dose. Em casos de vacinas de dose única ou vacinas que não precisarão ser tomadas novamente.
