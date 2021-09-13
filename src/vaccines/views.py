543729e9e9be42650a2113610148a256cc753400
Durval Carvalho <durvalcsouza@outlook.com>
2021-09-13 12:22:42 -0300

    #124 Configure VaccineViewSet to use IsPetOwner class permission

--- src/vaccines/views.py
+++ src/vaccines/views.py
@@ -1,28 +1,27 @@
-from rest_framework import serializers, viewsets
+from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.exceptions import ValidationError
 
+from animals.models import Pet
+from animals.permissions import IsPetOwner
+
 from vaccines.models import Vaccine
 from vaccines.serializers import VaccineSerializer
-from animals.models import Pet
 
 class VaccineViewSet(viewsets.ModelViewSet):
     queryset = Vaccine.objects.all()
     serializer_class = VaccineSerializer
     doc_tags = ["Vaccines"]
 
+    # Invés de verificar manualmente a permissão, em cada uma das controlers, criamos
+    # uma classe que encapsula uma permissão e deixamos o Django realizar a checagem de
+    # quem pode ou não pode acessar o recurso da url
+    permission_classes = [ IsPetOwner ]
+
     def get_queryset(self):
         pet = get_object_or_404(Pet, pk=self.kwargs["pet_pk"])
-        user = self.request.user
-        if (pet.owner == user):
-            queryset = pet.vaccines.all()
-            return queryset
-        else:
-            raise ValidationError("Este animal não é seu.")
-
+        return pet.vaccines.all()
 
     def perform_create(self, serializer):
         pet = get_object_or_404(Pet, pk=self.kwargs["pet_pk"])
         serializer.save(pet=pet)
-
-    
