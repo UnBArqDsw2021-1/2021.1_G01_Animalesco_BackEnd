81dee298667a1e2ed9f401c54768fa1fa6d535ba
Hugo Sobral <hugosobral10@gmail.com>
2021-09-13 11:13:50 -0300

    Improve vaccines models

--- src/vaccines/models.py
+++ src/vaccines/models.py
@@ -4,12 +4,15 @@ from animals.models import Pet
 class Vaccine(models.Model):
     name = models.CharField(verbose_name="Vaccine name", max_length=100, null=False, blank=False)
     application_date = models.DateField(verbose_name="Dose application date", null=False, blank=False)
-    next_application_date = models.DateField(verbose_name="Next dose application date")
+    next_application_date = models.DateField(verbose_name="Next dose application date", null=True, blank=True)
     pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name="vaccines")
 
     # Para que seja possível realizar um melhor controle gráfico das vacinas, este campo booleano informa se a vacina terá ou não
     # próxima dose. Em casos de vacinas de dose única ou vacinas que não precisarão ser tomadas novamente.
     is_finished = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('name', 'application_date', 'pet',)
 
     def __str__(self) -> str:
         return self.name
