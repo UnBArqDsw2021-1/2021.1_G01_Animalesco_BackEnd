60a75a95c32a4d72f070a9b6e6423cc8a46ac794
Durval Carvalho <durvalcsouza@outlook.com>
2021-09-24 10:14:19 -0300

    Add user admin panel

---
+++ src/authentication/admin.py
@@ -0,0 +1,20 @@
+# Register your models here.
+from django.contrib import admin
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from authentication.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        "id",
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_active",
+        "last_login",
+        "date_joined",
+    )
+    search_fields = ("email", "username", "first_name", )
