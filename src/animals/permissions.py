ab06eef2700df109d5f3f797a503ff4d334e6a92
Durval Carvalho <durvalcsouza@outlook.com>
2021-09-26 23:35:21 -0300

    Add Related Obj Pet Owner Permission class

--- src/animals/permissions.py
+++ src/animals/permissions.py
@@ -1,10 +1,10 @@
 from rest_framework.generics import get_object_or_404
-from rest_framework import permissions as perms
+from rest_framework import permissions
 
 from animals.models import Pet
 
 
-class IsPetOwner(perms.BasePermission):
+class IsPetOwner(permissions.BasePermission):
     """
     Classe usada para verificar se o usuário logado é dono do pet que ele
     está tentando interagir.
@@ -19,3 +19,14 @@ class IsPetOwner(perms.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user == obj.owner
 
+
+class IsRelatedPetOwner(permissions.BasePermission):
+
+    message = 'Action rejected! To perform this action, the logged in user must own the Pet.'
+
+    def has_permission(self, request, view):
+        pet = get_object_or_404(Pet, pk=view.kwargs["pet_pk"])
+        return pet.owner == request.user
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.pet.owner
